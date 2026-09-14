ec241c0b612742aa30cb9a96ed53807233fc54de
EmmaBruno <emmanuelbruno88@gmail.com>
2026-09-14 20:27:07 -0300

    Update Libro.java

--- src/main/java/ar/edu/biblioteca/Libro.java
+++ src/main/java/ar/edu/biblioteca/Libro.java
@@ -5,6 +5,7 @@ public class Libro {
   private String isbn;
   private String tìtulo;
   private boolean disponible;
+  private boolean postgrado;
 
   public Libro(String isbn, String titulo) {
     this.isbn = isbn;
